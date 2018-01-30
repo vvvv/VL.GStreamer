cf0a063f522daf97d72cbefe3c0fbe7f0c1459e2
Elias Holzer <elias@vvvv.org>
2018-01-31 00:05:59 +0100

    Running in 32bit should also work now.

--- src/Player.cs
+++ src/Player.cs
@@ -19,7 +19,8 @@ namespace VL.Lib.GStreamer
             // Initialize Gstreamer
             var args = System.Array.Empty<string>();
             var path = Environment.GetEnvironmentVariable("PATH") ?? String.Empty;
-            Environment.SetEnvironmentVariable("PATH", path + Path.PathSeparator + @"C:\gstreamer\1.0\x86_64\bin");
+            var folder = IntPtr.Size == 4 ? "x86" : "x86_64";
+            Environment.SetEnvironmentVariable("PATH", path + Path.PathSeparator + $@"C:\gstreamer\1.0\{folder}\bin");
             Application.Init(ref args);
             System.Windows.Forms.Application.Idle += Application_Idle;
         }
